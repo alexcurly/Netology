be1430493d4bce213bcdff4b30280015aba995f6
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2022-11-18 09:19:35 +0300

    Update main.java

--- Java_Basis/HashMap_PhoneBook/main.java
+++ Java_Basis/HashMap_PhoneBook/main.java
@@ -1,1 +1,50 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PhoneBook contacts = new PhoneBook();
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            fillGroups(contacts, scanner);
+            fillContacts(contacts, scanner);
+        }
+
+        System.out.println(contacts)
+    }
+
+    private static void fillGroups(PhoneBook contacts, Scanner scanner){
+        String tmpInput = null;
+        while (true) {
+            System.out.println("Введите название группы или end");
+            tmpInput = scanner.nextLine();
+            if(tmpInput.equals("end")) {
+                break;
+            }
+
+            contacts.addGroup(tmpInput);
+        }
+    }
+  
+    private static void fillContacts(PhoneBook contacts, Scanner scanner){
+        String tmpInput = null;
+        while (true) {
+            System.out.println("Введите имя, номер телефона [Имя 89819619704] или end");
+            tmpInput = scanner.nextLine();
+            if(tmpInput.equals("end")) {
+                break;
+            }
+            final String[] contactData = tmpInput.split("\\s+");
+            Contact contact = new Contact(contactData[0], contactData[1]);
+
+            System.out.println("Введите имя групп, в которые необходимо добавить контакт ");
+            String[] groupNames = scanner.nextLine().split("\\s+");
+
+            contacts.addContact(contact, groupNames);
+
+        }
+    }
+}

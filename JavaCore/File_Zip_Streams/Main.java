2c991e9b8cd7e3af28354c481f8a61b2b63cf6cc
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2022-12-02 11:41:46 +0300

    Update Main.java

--- JavaCore/File_Zip_Streams/Main.java
+++ JavaCore/File_Zip_Streams/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.*;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -14,37 +15,27 @@ public class Main {
     public static void main(String[] args) {
 
         StringBuilder history = new StringBuilder();
-
-
-
 //Создание Папок и Файлов
-        history.append(
-                folderCreator("C://Games"));
-        history.append(
-                folderCreator("C://Games//src"));
-        history.append(
-                folderCreator("C://Games//res"));
-        history.append(
-                folderCreator("C://Games//savegames"));
-        history.append(
-                folderCreator("C://Games//temp"));
+        //Сформируем лист с древом каталогов которые необходимо создать
+        List<String> foldersTree = Arrays.asList(
+                "C://Games",
+                "C://Games//src",
+                "C://Games//res",
+                "C://Games//savegames",
+                "C://Games//temp",
+                "C://Games//src//main",
+                "C://Games//src//test",
+                "C://Games//res//drawables",
+                "C://Games//res//vectors",
+                "C://Games//res//icons");
+        // Создадим папки
+        for (int i = 0; i <= (foldersTree.size() - 1); i++ ) {
+            history.append(folderCreator(foldersTree.get(i)));
+        }
+
         fileCreator("C://Games//temp", "temp.txt");
-
-        history.append(
-                folderCreator("C://Games//src//main"));
-        history.append(
-                fileCreator("C://Games//src//main", "Main.java"));
-        history.append(
-                fileCreator("C://Games//src//main", "Utils.java"));
-
-        history.append(
-                folderCreator("C://Games//src//test"));
-        history.append(
-                folderCreator("C://Games//res//drawables"));
-        history.append(
-                folderCreator("C://Games//res//vectors"));
-        history.append(
-                folderCreator("C://Games//res//icons"));
+        fileCreator("C://Games//src//main", "Main.java");
+        fileCreator("C://Games//src//main", "Utils.java");
 
         // Запись данных в файл temp.txt
         try (FileWriter tempWrite = new FileWriter("C://Games//temp//temp.txt", false)) {
@@ -64,8 +55,7 @@ public class Main {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-
-
+        
         // Создание трех экземпляров класса GameProgress
         GameProgress Hero1 = new GameProgress(100, 1, 1, 100);
         GameProgress Hero2 = new GameProgress(150, 2, 2, 150);
@@ -77,69 +67,6 @@ public class Main {
         saveGame("C://Games//savegames", Hero3);
 
         zipFiles("C://Games//savegames", saveFileList);
-
-
-//
-//        File folderGame = new File("C://Games");
-//        folderGame.mkdir();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//
-//        File folderGameSrc = new File("C://Games//src");
-//        folderGameSrc.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameSavegames = new File("C://Games//savegames");
-//        folderGameSavegames.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameTemp = new File("C://Games//temp");
-//        folderGameTemp.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameRes = new File("C://Games//res");
-//        folderGameRes.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameResIcons = new File("C://Games//res//icons");
-//        folderGameResIcons.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameResVectors = new File("C://Games//res//vectors");
-//        folderGameResVectors.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameResDrawables = new File("C://Games//res//drawables");
-//        folderGameResDrawables.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File folderGameSrcMain = new File("C://Games//src//main");
-//        folderGameSrcMain.mkdirs();
-//        history.append("\nДобавлена папка - . Директория ");
-//
-//        File fileMain = new File("C://Games//src//main//Main.java");
-//        try {
-//            if (fileMain.createNewFile())
-//                System.out.println("Файл был создан");
-//        } catch (IOException ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//
-//        File fileUtils = new File("C://Games//src//main//Utils.java");
-//        try {
-//            if (fileUtils.createNewFile())
-//                System.out.println("Файл был создан");
-//        } catch (IOException ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//
-//        File filetemp = new File("C://Games//temp//temp.txt");
-//        try {
-//            if (filetemp.createNewFile())
-//                System.out.println("Файл был создан");
-//        } catch (IOException ex) {
-//            System.out.println(ex.getMessage());
-//        }
     }
 
     public static String folderCreator(String directoryName) {
@@ -184,31 +111,6 @@ public class Main {
 
     public static void zipFiles(String zipFileDirectory, List<String> saveFileList) {
 
-//        try {
-//            FileOutputStream zipFile = new FileOutputStream("zipOfSaveFiles.zip");
-//            ZipOutputStream zipOut = new ZipOutputStream(zipFile);
-//            for(int i = 0; i < saveFileList.size(); i++ ) {
-//
-//                //выбираем фаил который будем архивировать
-//                FileInputStream filesForZip = new FileInputStream(saveFileList.get(i)) {
-//
-//                    ZipEntry entry = new ZipEntry(saveFileList.get(i));
-//                    zipOut.putNextEntry();
-//                    // считываем содержимое файла в массив byte
-//                    byte[] buffer = new byte[filesForZip.available()];
-//                    filesForZip.read(buffer);
-//                    // добавляем содержимое к архиву
-//                     zipOut.write(buffer);
-//                    // закрываем текущую запись для новой записи
-//                    zipOut.closeEntry();
-//                    };
-//            }
-//            zipOut.close();
-//            zipFile.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         try { ZipOutputStream zout = new ZipOutputStream(new FileOutputStream( zipFileDirectory + "//" + "zipOfSaveFiles.zip"));
 
             for(int i = 0; i < saveFileList.size(); i++ ) {
@@ -233,30 +135,5 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        for (String s : saveFileList){
-//            System.out.println("\n" + s);
-//        }
-//
-//        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream( zipFileDirectory + "//" + "zipOfSaveFiles.zip"));
-//                //выбираем фаил который будем архивировать
-//             FileInputStream filesForZip = new FileInputStream(zipFileDirectory + "//" + saveFileList.get(0))) {
-//
-//
-//            ZipEntry entry = new ZipEntry(saveFileList.get(0));
-//            zout.putNextEntry(entry);
-//            // считываем содержимое файла в массив byte
-//            byte[] buffer = new byte[filesForZip.available()];
-//            filesForZip.read(buffer);
-//            // добавляем содержимое к архиву
-//            zout.write(buffer);
-//            // закрываем текущую запись для новой записи
-//            zout.closeEntry();
-//        } catch (Exception e) {
-//            e.getMessage();
-//        }
-
     }
-
-
-
 }

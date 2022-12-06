2984cb019d8a3a98c2c706bd66644e968ddb187c
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2022-12-06 20:25:00 +0300

    Update Main.java

--- JavaCore/LamdaFunctions/lambda_calculator/src/com/company/Main.java
+++ JavaCore/LamdaFunctions/lambda_calculator/src/com/company/Main.java
@@ -1,40 +1,7 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-
-        Calculator calc = Calculator.instance.get(); // не понял зачем именно так создавать объект класса
-        // когда можно было просто написать Calculator calc = new Calculator(); в чем смысл?
-//        int a = calc.plus.apply(1, 2);
-//        int b = calc.minus.apply(1,1);
-//        int c = calc.devide.apply(a, b);//Ошибка в результате деления на ноль.
-        // Исправил переменную devide в классе Calculator
-//        calc.println.accept(c);
-
-
-
-
-
-
-
-
-        List<String > list = new ArrayList<>();
-        list.add("one");
-        list.add("two");
-        list.add("7");
-        for(String ss : list){
-            System.out.println(ss);
-        }
-
-
-
+        Calculator calc = Calculator.instance.get();
     }
-
 }
-
-
